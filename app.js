a94ef35dd684796b8eac46704223e7495cfa82a7
Daniel Arellano <daniel.arellano@ticketmaster.com>
2017-06-29 14:47:42 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get('/', (req, res) => res.json({ hello: 'world' }));
+
+
+module.exports = app;
