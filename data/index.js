d929b0301844786e4ec56a4b53563ea0230222bd
Sagar Makwana <mmcghee@Sagars-MacBook-Pro.local>
2017-06-29 19:39:16 -0700

    ursa recommendations added

--- data/index.js
+++ data/index.js
@@ -4,7 +4,7 @@ const {
   ursa
 } = require('./sources');
 
-module.exports.getPersonalizedEvents = ({ tmUserId, fbUserId = '10154599707336563' /* gabo's id */, location }) =>
+module.exports.getPersonalizedEvents = ({ tmUserId = '492196944', fbUserId = '10154599707336563' /* gabo's id */, location }) =>
   new Promise((resolve, reject) => {
     resolve([]);
   });
