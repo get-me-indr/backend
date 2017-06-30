f36588d01ead8412c999ea5e558be5694c8e0cf9
Daniel Arellano <daniel.arellano@ticketmaster.com>
2017-06-29 22:54:46 -0700

    updates

--- data/sources/facebook.js
+++ data/sources/facebook.js
@@ -1,9 +1,10 @@
 require('isomorphic-fetch');
 
 module.exports = {
-  getMusic: fbUserId => {
-    return fetch(`https://graph.facebook.com/v2.9/${fbUserId}/music?access_token=EAAZAyAs3RUfkBAEfbNhbBAaAnFruil8koOyZAZAeQBld25c61rTD9ITAvhTrlkZAGBmecZBA5YomKoNPfw5LtmArIZBmqLsa48PPQ66SEqQZBZBobHkn3FcSPyShJYkz4NuXT95uepJppxKkEdxs8AWL07ymL5uf6rDwBY8i7u67ThgcqhxYRY66UqilXuzq7qMZD&pretty=0`)
+  getMusic: (fbUserId, fbAccessToken) => {
+    return fetch(`https://graph.facebook.com/v2.9/${fbUserId}/music?access_token=${fbAccessToken}&pretty=0`)
       .then(res => res.json())
-      .then(json => json.data);
+      .then(json => json.data.map(artist => artist.name))
+      .then(console.log);
     }
 };
