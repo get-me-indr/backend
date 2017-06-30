59a2c83f4536114fadddde7e97dbde3110162252
Allen Scheinhaus <allen.scheinhaus@gmail.com>
2017-06-29 21:44:20 -0700

    add interactions endpoint

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { getPersonalizedEvents } = require('./data');
+const { firebase } = require('./data/sources');
 
 const app = express();
 
 app.use(cors())
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
@@ -19,6 +20,19 @@ app.get('/', (req, res) => {
     .catch(error => {
       return res.json({ error });
     });
+});
+
+app.post('/interactions', (req, res) => {
+  const { userId, interaction } = req.body;
+  console.log({ userId, interaction });
+  firebase.insertUserInteraction(userId, interaction)
+    .then(result => {
+      console.log('interactions POST result', { result });
+      res.json({ result });
+    })
+    .catch(error => {
+      res.json({ error });
+    })
 });
 
 
